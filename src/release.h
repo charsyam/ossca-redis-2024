79aa5b6512476b761894532a97881c4cddf2a62e
jang-namu <ironhee8005@gmail.com>
2024-04-25 00:23:48 +0900

    feat: echochulheejang 추가

---
+++ src/release.h
@@ -0,0 +1,5 @@
+#define REDIS_GIT_SHA1 "bcaa2a4c"
+#define REDIS_GIT_DIRTY "      68"
+#define REDIS_BUILD_ID "namuui-MacBookAir.local-1713971763"
+#include "version.h"
+#define REDIS_BUILD_ID_RAW SERVER_NAME VALKEY_VERSION REDIS_BUILD_ID REDIS_GIT_DIRTY REDIS_GIT_SHA1
