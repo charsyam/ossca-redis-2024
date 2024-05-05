ab3e898bfc6989d2bcaba6b5d3891480c9744fe5
장수현 <76679995+suhyeon10@users.noreply.github.com>
2024-05-05 17:58:05 +0900

    echoSUHYEONCommand 추가

--- src/server.c
+++ src/server.c
@@ -4610,6 +4610,10 @@ void echoCommand(client *c) {
     addReplyBulk(c,c->argv[1]);
 }
 
+void echoSUHYEONCommand(client *c) {
+    addReplyBulk(c,c->argv[1]);
+}
+
 void timeCommand(client *c) {
     addReplyArrayLen(c,2);
     addReplyBulkLongLong(c, server.unixtime);
